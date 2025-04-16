c16a43913b8505e722e8653ed0233b306b72f812
maitd <maitd2010@gmail.com>
2025-04-16 17:18:08 +0700

    update

--- Client1/Pages/Home.razor.cs
+++ Client1/Pages/Home.razor.cs
@@ -1,88 +1,88 @@
-﻿using AntDesign.Charts;
-using Microsoft.AspNetCore.Components;
-using OfficeOpenXml.DataValidation;
-using Shared;
-using Shared.DTOs.ResponseModel;
+﻿//using AntDesign.Charts;
+//using Microsoft.AspNetCore.Components;
+//using OfficeOpenXml.DataValidation;
+//using Shared;
+//using Shared.DTOs.ResponseModel;
 
-namespace Client1.Pages
-{
-    public partial class Home : ComponentBase
-    {
-        [Inject]
-        private IClassService _classService{ get; set; } = null!;
-        [Inject]
-        private IStudentService _studentService{ get; set; } = null!;
+//namespace Client1.Pages
+//{
+//    public partial class Home : ComponentBase
+//    {
+//        [Inject]
+//        private IClassService _classService{ get; set; } = null!;
+//        [Inject]
+//        private IStudentService _studentService{ get; set; } = null!;
 
-        private List<TeacherChart> data = new List<TeacherChart>();
-        private List<ClassChart> data2 = new List<ClassChart>();
-
-
-        readonly BarConfig config1 = new BarConfig
-        {
-            Title = new Title
-            {
-                Visible = true,
-                Text = "Tổng các lớp"
-            },
-            XField = "values",
-            YField = "type"
-        };
-
-        readonly BarConfig config2 = new BarConfig
-        {
-            Title = new Title
-            {
-                Visible = true,
-                Text = "Tổng học sinh"
-            },
-            XField = "values",
-            YField = "type"
-        };
+//        private List<TeacherChart> data = new List<TeacherChart>();
+//        private List<ClassChart> data2 = new List<ClassChart>();
 
 
-        protected override async Task OnInitializedAsync()
-        {
-            try
-            {
+//        readonly BarConfig config1 = new BarConfig
+//        {
+//            Title = new Title
+//            {
+//                Visible = true,
+//                Text = "Tổng các lớp"
+//            },
+//            XField = "values",
+//            YField = "type"
+//        };
 
-                await LoadData();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error fetching students: {ex.Message}");
-            }
-        }
+//        readonly BarConfig config2 = new BarConfig
+//        {
+//            Title = new Title
+//            {
+//                Visible = true,
+//                Text = "Tổng học sinh"
+//            },
+//            XField = "values",
+//            YField = "type"
+//        };
 
-        private async Task LoadData()
-        {
-            var classes = await _classService.GetClassesAsync();
 
-            data = classes.GroupBy(x => x.TeacherName).Select(x => new TeacherChart
-            {
-                type = x.Key,
-                values = x.Count()
-            }).ToList();
+//        protected override async Task OnInitializedAsync()
+//        {
+//            try
+//            {
 
-            var students = await _studentService.GetStudentsAsync();
-            data2 = students.GroupBy(x => x.Class.Name).Select(x => new ClassChart
-            {
-                type = x.Key,
-                values = x.Count()
-            }).ToList();
+//                await LoadData();
+//            }
+//            catch (Exception ex)
+//            {
+//                Console.WriteLine($"Error fetching students: {ex.Message}");
+//            }
+//        }
 
-            StateHasChanged();
-        }
-    }
-    public class TeacherChart
-    {
-        public string type { get; set; }
+//        private async Task LoadData()
+//        {
+//            var classes = await _classService.GetClassesAsync();
 
-        public int values { get; set; }
-    }
-    public class ClassChart
-    {
-        public string type { get; set; }
-        public int values { get; set; }
+//            data = classes.GroupBy(x => x.TeacherName).Select(x => new TeacherChart
+//            {
+//                type = x.Key,
+//                values = x.Count()
+//            }).ToList();
 
-    }
-}
+//            var students = await _studentService.GetStudentsAsync();
+//            data2 = students.GroupBy(x => x.Class.Name).Select(x => new ClassChart
+//            {
+//                type = x.Key,
+//                values = x.Count()
+//            }).ToList();
+
+//            StateHasChanged();
+//        }
+//    }
+//    public class TeacherChart
+//    {
+//        public string type { get; set; }
+
+//        public int values { get; set; }
+//    }
+//    public class ClassChart
+//    {
+//        public string type { get; set; }
+//        public int values { get; set; }
+
+//    }
+//}
